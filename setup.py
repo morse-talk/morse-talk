4bb7a4d1bbb24cae6ed7bae0f38cb8842f464cbb
Himanshu Mishra <himanshu2014iit@gmail.com>
2015-11-28 16:42:10 +0530

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,25 @@
 from setuptools import setup, find_packages
-from os import path
 
-here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, 'DESCRIPTION.md')) as f:
-    long_description = f.read()
 
 setup(
-	name='morse-talk',
-	version='0.1.1',
-	author='Himanshu Mishra',
-	author_email='himanshu2014iit@gmail.com',
-	description='An aide to Morse Code',
-	long_description=long_description,
-	url='https://github.com/orkohunter/morse-talk',
-	download_url='https://github.com/orkohunter/morse-talk/tarball/v0.1',
-	license='MIT',
-	classifiers = [
-	'Development Status :: 4 - Beta',
-	'Programming Language :: Python :: 2',
-	'Programming Language :: Python :: 3',
-	],
-	keywords='morse code talk',
-	packages=[
-	'morse_talk'
-	]
-	)
+    name='morse-talk',
+    version='0.1.3',
+    author='Himanshu Mishra',
+    author_email='himanshu2014iit@gmail.com',
+    description='An aide to Morse Code',
+    url='https://github.com/orkohunter/morse-talk',
+        download_url='https://github.com/orkohunter/morse-talk/tarball/v0.1',
+        license='MIT',
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 3',
+        ],
+    keywords='morse code talk',
+    packages=[
+        'morse_talk'
+    ],
+    test_suite='nose.collector',
+    tests_require=['nose>=0.10.1']
+
+)
