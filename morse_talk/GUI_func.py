e9b307f1245cecd55659705784a6a0ebac71a7f3
aribis369 <aribis369@gmail.com>
2016-12-22 14:31:38 +0530

    GUI_func for morse-talk

---
+++ morse_talk/GUI_func.py
@@ -0,0 +1,69 @@
+from tkinter import *
+import time
+import morse_talk as mtalk
+import sys
+
+
+def encode():
+  global dc
+  x = e1.get()
+  q = mtalk.encode(x)
+  if q != dc:
+      dc = q
+      e3.delete(0, END)
+      e3.insert(0, q)
+  e3.after(500, encode)
+
+
+def decode():
+    global cd
+    global y
+    if e2.get() == '':
+        y = "a"
+    else:
+        y = e2.get()
+    z = ''
+    r = ""
+    if y[-1] == ' ':
+      r = mtalk.decode(y)
+      if r != cd:
+          cd = r
+          e4.delete(0, END)
+          e4.insert(0, r)
+    e4.after(500, decode)
+
+
+dc = ''
+cd = ''
+y = "a"
+master = Tk()
+master.title("morse_talk")
+
+Label(master, text="Encode", font=('Courier', 25, 'bold')).grid(row=3)
+Label(master, text="Decode", font=('Courier', 25, 'bold')).grid(row=11)
+Label(master, text="Encoded text", font=('Courier', 25, 'bold')).grid(row=6)
+Label(master, text="Decoded code", font=('Courier', 25, 'bold')).grid(row=14)
+
+e1 = Entry(master, font=('arial', 20, 'bold'))
+e2 = Entry(master, font=('arial', 20, 'bold'))
+e3 = Entry(master, font=('arial', 20, 'bold'))
+e4 = Entry(master, font=('arial', 20, 'bold'))
+
+e1.grid(row=3, column=1)
+e2.grid(row=11, column=1)
+e3.grid(row=6, column=1)
+e4.grid(row=14, column=1)
+
+
+def main():
+  l = input("execute_GUI (y/n)?")
+  if(l == "y"):
+    encode()
+    decode()
+    mainloop()
+  else:
+    print("prog_exited")
+
+ 
+if __name__ == "__main__":
+    main()
